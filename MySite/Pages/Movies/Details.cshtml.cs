fcee52de5696e0447232ce8f53c895f7e0f991b2
supertopdev <supertopdev@gmail.com>
2019-05-22 21:24:28 +0300

    update

---
+++ MySite/Pages/Movies/Details.cshtml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using MySite.Models;
+
+namespace MySite.Pages.Movies
+{
+    public class DetailsModel : PageModel
+    {
+        private readonly MySite.Models.MySiteContext _context;
+
+        public DetailsModel(MySite.Models.MySiteContext context)
+        {
+            _context = context;
+        }
+
+        public Movie Movie { get; set; }
+
+        public async Task<IActionResult> OnGetAsync(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            Movie = await _context.Movie.FirstOrDefaultAsync(m => m.ID == id);
+
+            if (Movie == null)
+            {
+                return NotFound();
+            }
+            return Page();
+        }
+    }
+}
---
+++ MySite/Pages/Movies/Index.cshtml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using MySite.Models;
+
+namespace MySite.Pages.Movies
+{
+    public class IndexModel : PageModel
+    {
+        private readonly MySite.Models.MySiteContext _context;
+
+        public IndexModel(MySite.Models.MySiteContext context)
+        {
+            _context = context;
+        }
+
+        public IList<Movie> Movie { get; set; }
+
+        public async Task OnGetAsync()
+        {
+            Movie = await _context.Movie.ToListAsync();
+        }
+    }
+}
